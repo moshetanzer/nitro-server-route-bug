9f8a4e15cfdbb6bbd70b496352b460c9f08c78b5
Moshe Tanzer <tanzermt@gmail.com>
2024-11-11 09:17:30 +0200

    chore: up

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const {data: broken} = await useFetch('/api/broken/testtoken', {
+  method: 'GET',
+  server: false
+})
+const {data: working} = await useFetch('/api/working/testtoken', {
+  method: 'GET',
+  server: false
+})
+</script>
+<template>
+  <div>
+    <div>broken - {{ broken }}</div>
+    <div>working - {{ working }}</div>
+  </div>
+</template>
